5b21699f906a0de0d4ce2ef53e30c92218c0e4a5
luisfb <luis_armandob@yahoo.com.br>
2025-11-28 19:23:32 -0300

    com procfile

--- index.py
+++ index.py
@@ -50,6 +50,8 @@ def display_page(pathname):
             html.P(f"Rota inválida: {pathname}")
         ])
 
+import os
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(host="0.0.0.0", port=port, debug=False)
